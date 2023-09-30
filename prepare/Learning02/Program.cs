de475ae2b20d78f663f2feecefe7e2bdedfa9386
HaroldBarraza <132396790+HaroldBarraza@users.noreply.github.com>
2023-09-30 10:04:13 -0500

    task

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Collections.Generic;
+
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
+public class Resume
+{
+    public string _personName;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Resume of {_personName}");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create job instances
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create a resume instance
+        Resume myResume = new Resume();
+        myResume._personName = "John Doe";
+
+        // Add jobs to the resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display the resume details
+        myResume.Display();
     }
 }
--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,123 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference _reference = new Reference("John 3:16");
+        Scripture _scripture = new Scripture(_reference, "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+        
+        while (!_scripture.AreWordsHidden())
+        {
+            _scripture.DisplayScripture();
+            Console.WriteLine("Press Enter to continue or type 'quit' to exit:");
+            string user = Console.ReadLine();
+
+            if (user.ToLower() == "quit")
+            {
+                break;
+            }
+            _scripture.HideRandomWord();
+            Console.Clear();   
+        }
+    }
+}
+
+class Scripture
+{
+    private Reference _reference;
+    private string _text;
+    private ScriptureWord[] _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _text = text;
+        InitializeWords();
+    }
+
+    private void InitializeWords()
+    {
+        string[] wordArray = _text.Split(' ');
+        _words = new ScriptureWord[wordArray.Length];
+        for (int i = 0; i < wordArray.Length; i++)
+        {
+            _words[i] = new ScriptureWord(wordArray[i]);
+        }
+    }
+
+    public void HideRandomWord()
+    {
+        Random random = new Random();
+        int index = random.Next(_words.Length);
+        _words[index].Hide();
+    }
+
+    public bool AreWordsHidden()
+    {
+        foreach (ScriptureWord word in _words)
+        {
+            if (!word.IsHidden)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void DisplayScripture()
+    {
+        Console.WriteLine(_reference.GetReferenceText());
+        foreach (ScriptureWord word in _words)
+        {
+            Console.Write(word.GetDisplayText() + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+class Reference
+{
+    private string _referenceText;
+
+    public Reference(string referenceText)
+    {
+        _referenceText = referenceText;
+    }
+
+    public string GetReferenceText()
+    {
+        return _referenceText;
+    }
+}
+
+class ScriptureWord
+{
+    private string _text;
+    private bool _isHidden;
+
+    public ScriptureWord(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public bool IsHidden
+    {
+        get { return _isHidden; }
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        {
+            return "______";
+        }
+        else
+        {
+            return _text;
+        }
     }
 }
